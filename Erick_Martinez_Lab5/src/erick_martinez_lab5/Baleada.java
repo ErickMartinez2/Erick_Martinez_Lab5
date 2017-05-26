4aef622bcfd436b9bbef5b40c973e376a73c212a
Erick Martinez <Erick Martinez>
2017-05-26 13:32:46 -0600

    Clases y main creadas

---
+++ Erick_Martinez_Lab5/src/erick_martinez_lab5/Baleada.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erick_martinez_lab5;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author usuario
+ */
+public class Baleada {
+    public ArrayList<String> ingrediente = new ArrayList();
+    
+}
---
+++ Erick_Martinez_Lab5/src/erick_martinez_lab5/Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erick_martinez_lab5;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author usuario
+ */
+public class Cliente {
+    public String nombre;
+    public String numero;
+    public double dinero;
+    public String direccion;
+    public ArrayList<Baleada> baleada = new ArrayList();
+}
